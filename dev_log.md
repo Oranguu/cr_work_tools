c3ea9897612f47bb807c05bf7fa73116d49cbb39
chenrui <108974289@qq.com>
2024-11-07 13:46:44 +0800

    午间学习

--- dev_log.md
+++ dev_log.md
@@ -2,5 +2,4 @@
 
 # 2024.11.7
 
-
-
+基于https://doc.qt.io/qtforpython-6/tutorials/index.html进行学习，在slot and signal这一节会有一些卡顿，需要继续逐字梳理。
--- tutorial/simple_button.py
+++ tutorial/simple_button.py
@@ -1,3 +1,24 @@
 import sys
 from PySide6.QtWidgets import QApplication, QPushButton
 from PySide6.QtCore import Slot
+
+# Greetings--Slots can be used for receiving signals,
+@Slot()
+def say_hello():
+    print("Button clicked, Hello!")
+
+# Create the Qt Application
+app = QApplication(sys.argv)
+
+# Create a button
+button = QPushButton("Click me")
+
+# Connect the button to the function---this is the singal---control the slot
+button.clicked.connect(say_hello)
+
+# Show the button
+button.show()
+
+# Run the main Qt loop
+app.exec()
+
